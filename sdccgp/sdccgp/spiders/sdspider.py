a60f7c293d0ca524227a88a81a0d44dbd6e8622a
xieyafei <1244790351@qq.com>
2018-09-05 23:22:25 +0800

    在starts_request中，使用selenium来生成所有的下一级的url

--- sdccgp/sdccgp/spiders/sdspider.py
+++ sdccgp/sdccgp/spiders/sdspider.py
@@ -6,6 +6,7 @@ from scrapy import spider,Request
 from selenium.common.exceptions import TimeoutException
 from scrapy.http import HtmlResponse
 import time
+from lxml import etree
 
 
 class SdspiderSpider(scrapy.Spider):
@@ -19,26 +20,24 @@ class SdspiderSpider(scrapy.Spider):
 
 
     def start_requests(self):
-        """使用selenium"""
+        """使用selenium翻页并生成详情页链接"""
         start_url = ['http://ccgp-shandong.gov.cn/sdgp2017/site/channelall.jsp?colcode=0301']
         for url in start_url:
             self.brower.get(url)
-            self.get_next_page()
-
-
-    def get_next_page(self):
-        urls = [note.xpath('a/@href').extract() for note in self.brower.find_elements_by_xpath('//td[@class="Font9"]')[:-1]]
-        for url in  urls:
-            yield Request(url=url)
-        time.sleep(3)
-        try:
-            self.brower.find_element_by_link_text('下一页').click()
-            self.get_next_page()
-        except:
-            print("爬取结束")
-
-
-
+            while self.brower.find_element_by_link_text('下一页'):
+                html = etree.HTML(self.brower.page_source)
+                for note in html.xpath('//td[@class="Font9"]')[:-1]:
+                    #构建url并将项目名称和时间作为参数传入
+                    note_url = note.xpath('a/@href')[0]
+                    note_name = note.xpath('a/text()')[0].replace(" ","")
+                    note_time = note.xpath('text()')[3].replace(' ','')
+                    url = 'http://ccgp-shandong.gov.cn' + note_url + '&note_name='
+                    yield Request(url=url)
+                self.brower.find_element_by_link_text('下一页').click()
+            for note in self.brower.find_elements_by_xpath('//td[@class="Font9"]')[:-1]:
+                url = 'http://ccgp-shandong.gov.cn' + note.xpath('a/@href')[0]
+                yield Request(url=url)
+            print('爬取一个分类')
 
 
     def closed(self,spider):
@@ -46,10 +45,18 @@ class SdspiderSpider(scrapy.Spider):
         self.brower.close()
 
 
-    def parse_response(self,response):
+    def parse(self,response):
         print('开始获取数据')
         item = SdccgpItem()
-        item['name'] = '完成'
+        item['uuid'] =
+        item['name'] =
+        item['href'] = response.url
         print('完成获取数据')
 
         yield item
+
+
+
+
+
+
